bd4d7cc97400fcbe7068561b27bd5f53d27262dd
lucasmcar <lmc.lucas90@gmail.com>
2021-01-12 16:45:56 -0300

    Projeto Scanner C#

---
+++ Scanner/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿
+namespace Scanner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnWia = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnWia
+            // 
+            this.btnWia.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnWia.Image = ((System.Drawing.Image)(resources.GetObject("btnWia.Image")));
+            this.btnWia.Location = new System.Drawing.Point(86, 235);
+            this.btnWia.Name = "btnWia";
+            this.btnWia.Size = new System.Drawing.Size(139, 127);
+            this.btnWia.TabIndex = 0;
+            this.btnWia.UseVisualStyleBackColor = true;
+            this.btnWia.Click += new System.EventHandler(this.btnWia_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(53, 151);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(217, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Clique para scannear";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCyan;
+            this.ClientSize = new System.Drawing.Size(308, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnWia);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnWia;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Scanner/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Scanner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnWia_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var dialog = new WIA.CommonDialogClass();
+                WIA.Device scanner = dialog.ShowSelectDevice(WIA.WiaDeviceType.ScannerDeviceType, true, false);
+                dialog.ShowAcquisitionWizard(scanner);
+            }
+            catch
+            {
+
+                MessageBox.Show("Não foi possível scannear documento");
+            }
+        }
+    }
+}
